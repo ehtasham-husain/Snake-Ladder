d5caa0e7569310592e3aecabea24c6f1229da34f
Ehtasham <ehtashamhusain@gmail.com>
2021-05-29 14:53:17 +0530

    UC-2 Player rolls the dice and get a number btw 0 to 6

--- src/com/snakeladder/PlaySnakeLadder.java
+++ src/com/snakeladder/PlaySnakeLadder.java
@@ -2,7 +2,14 @@ package com.snakeladder;
 
 public class PlaySnakeLadder {
     int player1_Position = 0;
+
+    public void RollsDice(){
+        double diceNum = Math.floor(Math.random() *10)%7;
+        System.out.println(diceNum);
+    }
     public static void main(String[] args) {
+        PlaySnakeLadder playSnakeLadder = new PlaySnakeLadder();
+        playSnakeLadder.RollsDice();
 
     }
 }
